e8aecc4e255e574351e16b3f3dd527e7a9ccfe54
Miguel Teixeira <147268449+Miguel170307@users.noreply.github.com>
2025-02-25 19:49:00 -0300

    exemplo3.php

---
+++ exemplo3.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color: black;
+        }
+        body{
+            background : pink
+        }
+    </style>
+</head>
+<body>
+<?php
+    
+    $raio = 10;
+    
+     $area = 3.14 * $raio * $raio;
+     echo " <h1> Área do círculo</h1>";
+     echo " O número apresentado é <strong> 10 </strong> <br>";
+     echo " A área é:" . $area . "<br>";
+?>    
+</body>
+</html>
