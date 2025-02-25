ad6860399a2d4bc5ea64764d9307c1a0e411d220
Miguel Teixeira <147268449+Miguel170307@users.noreply.github.com>
2025-02-25 19:48:24 -0300

    exemplo2.php

---
+++ exemplo2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color: black;
+        }
+        body{
+            background : green
+        }
+    </style>
+</head>
+<body>
+<?php
+    
+    $val = 20;
+    
+
+    $antec = $val - 1;
+    $suc = $val + 1;
+    
+    
+    echo " <h1> Antecessor e Sucessor </h1>";
+    echo " O número apresentado é <strong> 20 </strong> <br>";
+    echo " O antecessor é:" . $antec . "<br>";
+    echo " O sucessor é:" . $suc. "<br>";
+    ?>  
+</body>
+</html>
+
+    
