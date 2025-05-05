1be6d2e0b1635f3c4df0fdc34470e70cffdf342c
Miguel Teixeira <147268449+Miguel170307@users.noreply.github.com>
2025-05-05 06:34:14 -0300

    exercicio11.dart

---
+++ exercicio11.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void main() {
+  stdout.write('Informe a primeira nota: ');
+  double not1 = double.parse(stdin.readLineSync()!);
+
+  stdout.write('Informe a segunda nota: ');
+  double not2 = double.parse(stdin.readLineSync()!);
+
+  stdout.write('Informe a terceira nota: ');
+  double not3 = double.parse(stdin.readLineSync()!);
+
+  stdout.write('Informe a quarta nota: ');
+  double not4 = double.parse(stdin.readLineSync()!);
+
+  double media = (not1 + not2 + not3 + not4) / 4;
+
+  if (media >= 5) {
+    print('Aluno aprovado, média: $media');
+  } else {
+    print('Aluno reprovado, média: $media');
+  }
+}
